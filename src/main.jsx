68b502e6809a10b62a455cc5999660ada07ac548
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-07-10 00:35:44 +0530

    Add the rendered image component from RenderImages to the main JSX file.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { queryImage } from './GetImages'
+import { RenderImages } from './RenderImages'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RenderImages resPromise={queryImage('nature', 90)} />
   </React.StrictMode>,
 )
