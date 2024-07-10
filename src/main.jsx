897f2036961128edf0721e7e77086065655f7339
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-07-10 23:25:08 +0530

    Add a parameter for number of pages to be rendered in the main file component.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { queryImage } from './GetImages'
-import { RenderImages } from './RenderImages'
+import { RenderImages } from './RenderImages/RenderImages'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RenderImages resPromise={queryImage('nature', 90)} />
+    <RenderImages resPromise={queryImage('snow', 90)} />
   </React.StrictMode>,
 )
